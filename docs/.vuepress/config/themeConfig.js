510e7554c1479253b5a404d888d314fa1b23179b
lvan <155333767@qq.com>
2021-11-01 20:28:16 +0800

    update

--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -4,8 +4,8 @@ const nav = require('./nav.js');
 module.exports = {
     nav,
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
-    logo: '/img/EB-logo.png', // 导航栏logo
-    repo: 'xugaoyi/vuepress-theme-vdoing', // 导航栏右侧生成Github链接
+    logo: 'https://avatars.githubusercontent.com/u/19908635?v=4', // 导航栏logo
+    repo: 'LvanLiu/LvanNote', // 导航栏右侧生成Github链接
     searchMaxSuggestions: 10, // 搜索结果显示最大数
     lastUpdated: '上次更新', // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
     docsDir: 'docs', // 编辑的文件夹
@@ -26,8 +26,6 @@ module.exports = {
         slogan: '一个擅长写BUG的Java工程师',
     },
     social: {
-        // 社交图标，显示于博主信息栏和页脚栏
-        // iconfontCssFile: '//at.alicdn.com/t/font_1678482_u4nrnp8xp6g.css', // 可选，阿里图标库在线css文件地址，对于主题没有的图标可自由添加
         icons: [
             {
                 iconClass: 'icon-youjian',
@@ -43,8 +41,8 @@ module.exports = {
     },
     footer: {
         // 页脚信息
-        createYear: 2019, // 博客创建年份
+        createYear: 2021, // 博客创建年份
         copyrightInfo:
-            'Evan Xu | <a href="https://github.com/xugaoyi/vuepress-theme-vdoing/blob/master/LICENSE" target="_blank">MIT License</a>', // 博客版权信息，支持a标签
+            'Lvan liu | <a href="https://github.com/LvanLiu/LvanNote/blob/add-license-1/LICENSE" target="_blank">GUN License</a>', // 博客版权信息，支持a标签
     },
 }
