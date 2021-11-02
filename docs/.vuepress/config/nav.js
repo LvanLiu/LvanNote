a236bc1c5eddd38fbda3b77f5a076ec6f1191a48
lvan <155333767@qq.com>
2021-11-02 19:54:11 +0800

    新增导航栏

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -1,3 +1,48 @@
 module.exports = [
-  { text: '首页', link: '/' }
+  { text: '首页', link: '/' },
+  {
+    text: '后端',
+    link: '/core/', 
+    items: [],
+  },
+  {
+    text: '程序设计',
+    link: '/design/',
+    items: [],
+  },
+  {
+    text: '计算机基础',
+    link: '/basic/',
+    items: [],
+  },
+  {
+    text: '更多',
+    link: '/more/',
+    items: [
+      { text: '学习', link: '/pages/f2a556/' },
+      { text: '面试', link: '/pages/aea6571b7a8bae86/' },
+      { text: '心情杂货', link: '/pages/2d615df9a36a98ed/' },
+      { text: '实用技巧', link: '/pages/baaa02/' },
+      { text: '友情链接', link: '/friends/' },
+    ],
+  },
+  { text: '关于', link: '/about/' },
+  {
+    text: '收藏',
+    link: '/pages/beb6c0bd8a66cea6/',
+    // items: [
+    //   { text: '网站', link: '/pages/beb6c0bd8a66cea6/' },
+    //   { text: '资源', link: '/pages/eee83a9211a70f9d/' },
+    //   { text: 'Vue资源', link: '/pages/12df8ace52d493f6/' },
+    // ],
+  },
+  {
+    text: '索引',
+    link: '/archives/',
+    items: [
+      { text: '分类', link: '/categories/' },
+      { text: '标签', link: '/tags/' },
+      { text: '归档', link: '/archives/' },
+    ],
+  },
 ]
