ebe462d7408be188c5331fd40f622c6831a7f4f4
lvan <155333767@qq.com>
2021-11-02 23:04:54 +0800

    更新配置

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -2,7 +2,7 @@ module.exports = [
   { text: '首页', link: '/' },
   {
     text: '后端',
-    link: '/core/', 
+    link: '/core/',
     items: [],
   },
   {
@@ -15,27 +15,7 @@ module.exports = [
     link: '/basic/',
     items: [],
   },
-  {
-    text: '更多',
-    link: '/more/',
-    items: [
-      { text: '学习', link: '/pages/f2a556/' },
-      { text: '面试', link: '/pages/aea6571b7a8bae86/' },
-      { text: '心情杂货', link: '/pages/2d615df9a36a98ed/' },
-      { text: '实用技巧', link: '/pages/baaa02/' },
-      { text: '友情链接', link: '/friends/' },
-    ],
-  },
   { text: '关于', link: '/about/' },
-  {
-    text: '收藏',
-    link: '/pages/beb6c0bd8a66cea6/',
-    // items: [
-    //   { text: '网站', link: '/pages/beb6c0bd8a66cea6/' },
-    //   { text: '资源', link: '/pages/eee83a9211a70f9d/' },
-    //   { text: 'Vue资源', link: '/pages/12df8ace52d493f6/' },
-    // ],
-  },
   {
     text: '索引',
     link: '/archives/',
